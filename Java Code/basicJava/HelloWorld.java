3be5029aa583af716c545201e8c0576eab591fb3
vasanth2030 <vasanths2030@gmail.com>
2022-10-28 21:36:15 +0530

    day1

---
+++ Java Code/basicJava/HelloWorld.java
@@ -0,0 +1,15 @@
+package basicJava;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		String name;      //declaration
+		name="Vasanth";   //initialization
+		int age=21;
+		double height=178.5;
+		
+		System.out.println("Hello World");	//sysout
+		System.out.println("Name: "+name);
+		System.out.println("Age: "+age);
+		System.out.println("Height: "+height+"cm");
+	}
+}
