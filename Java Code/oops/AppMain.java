4ac8d538ff8aacfbde8886f4a03370d7a7c19585
vasanth2030 <vasanths2030@gmail.com>
2022-11-10 21:09:17 +0530

    day8

---
+++ Java Code/oops/AppMain.java
@@ -0,0 +1,12 @@
+package oops;
+
+public class AppMain {
+	public static void main(String[] args) {
+		AppVersion2 v2= new AppVersion2();
+		v2.function1();
+		v2.function2();
+		v2.function3();
+		v2.function4();
+		v2.function5();
+	}
+}
---
+++ Java Code/oops/AppVersion2.java
@@ -0,0 +1,12 @@
+package oops;
+
+public class AppVersion2 extends AppVersion1{
+	public void function4()
+	{
+		System.out.println("Doing function 4");
+	}
+	public void function5()
+	{
+		System.out.println("Doing function 5");
+	}
+}
